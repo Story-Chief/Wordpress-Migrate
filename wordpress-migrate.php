339ad4c9c78f700a3e91c7720f98c21291f51579
Bastiaan Dewaele <bastiaan@storychief.io>
2021-07-23 10:21:10 +0200

    test

---
+++ wordpress-migrate.php
@@ -0,0 +1,11 @@
+<?php 
+
+/**
+ * Plugin Name: StoryChief WPML
+ * Plugin URI: https://storychief.io/wordpress-wpml
+ * Description: This plugin lets StoryChief and WPML work together.
+ * Version: 1.0.4
+ * Author: Gregory Claeyssens
+ * Author URI: http://storychief.io
+ * License: GPL2
+ */
